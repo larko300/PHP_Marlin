2b61b151e45ca8e369d339b60d22985dbf26a366
larko300 <54770910+larko300@users.noreply.github.com>
2019-09-13 16:26:51 +0200

    Add files via upload

---
+++ bd_register.php
@@ -0,0 +1,18 @@
+<?php
+$servername = "localhost";
+$username = "larko3000";
+$password = "07081997A";
+$dbname = "blog";
+
+$username_register = $_POST['name'];
+$user_email = $_POST['email'];
+$user_password = $_POST['password'];
+
+if (!empty($username_register)&&!empty($user_email)&&!empty($user_password)) {
+  $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  $sql = "INSERT INTO register (username, email, password)
+  VALUES ('$username_register', '$user_email', '$user_password')";
+  $conn->exec($sql);
+}
+?>
