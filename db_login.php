d3c85faeb93914add0caaf2c79aa7732cf50fb81
larko300 <54770910+larko300@users.noreply.github.com>
2019-09-16 18:21:10 +0200

    Add files via upload

---
+++ db_login.php
@@ -0,0 +1,36 @@
+<?php
+$servername = "localhost";
+$username = "larko3000";
+$password = "07081997A";
+$dbname = "blog";
+
+$user_email = $_POST['email'];
+$user_password = $_POST['password'];
+
+$conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+$sql = "SELECT * FROM register WHERE email = '$user_email'";
+$statement = $conn->query($sql);
+$db_user_data = $statement->fetch(PDO::FETCH_ASSOC);
+$user_db_email = $db_user_data['email'];
+$user_db_password = $db_user_data['password'];
+$user_db_email = $db_user_data['username'];
+$password_verify = password_verify($user_password, $user_db_password);
+session_start();
+
+if ($password_verify==true&&$user_email==$user_db_email) {
+  $_SESSION['email_varify'] = $user_email;
+  $_SESSION['name_varify'] = $user_db_name;
+  header("location:http://localhost/Marlin_Materialy/logined.php");
+}
+elseif ($user_email!==$user_db_email) {
+  $_SESSION['flash_verify_email'] = 'Емайл указан неверно';
+  header("location:http://localhost/Marlin_Materialy/login.php");
+}
+elseif ($password_verify==false) {
+  $_SESSION['flash_verify_password'] = 'пароль указан неверно';
+  header("location:http://localhost/Marlin_Materialy/login.php");
+}
+
+
+
+?>
